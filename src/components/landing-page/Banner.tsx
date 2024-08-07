42da73e4ab78fd586ae0b51493f1b517894b6769
eunjae <rladmswo1715@naver.com>
2024-08-07 22:51:05 +0900

    Fix: 메인페이지 CSS수정

--- src/components/landing-page/Banner.tsx
+++ src/components/landing-page/Banner.tsx
@@ -37,6 +37,7 @@ export default function Banner() {
 
   useEffect(() => {
     if (bannerRef.current !== null) {
+      bannerRef.current.style.transition = 'all 0.5s ease-in-out';
       bannerRef.current.style.transform = `translateX(-${currentIndex}00%)`;
     }
   }, [currentIndex]);
@@ -44,56 +45,55 @@ export default function Banner() {
   useEffect(() => {
     const intervalId = setInterval(() => {
       if (!isHover) {
-        const newIndex = currentIndex + 1;
-
-        if (newIndex === 4) {
-          moveToNthBanner(1);
-        }
-
-        setCurrentIndex((prevCurrentIndex) => prevCurrentIndex + 1);
-
-        if (bannerRef.current !== null) {
-          bannerRef.current.style.transition = 'all 0.5s ease-in-out';
-        }
+        setCurrentIndex((prevCurrentIndex) => {
+          const newIndex = prevCurrentIndex + 1;
+          if (newIndex === 4) {
+            setTimeout(() => {
+              if (bannerRef.current !== null) {
+                bannerRef.current.style.transition = 'none';
+                bannerRef.current.style.transform = 'translateX(-100%)';
+              }
+              setCurrentIndex(1);
+            }, 500);
+            return newIndex;
+          }
+          return newIndex;
+        });
       }
     }, 5000);
 
     return () => clearInterval(intervalId);
-  }, [currentIndex, isHover]);
-
-  const moveToNthBanner = (index: number) => {
-    setTimeout(() => {
-      setCurrentIndex(index);
-      if (bannerRef.current !== null) {
-        bannerRef.current.style.transition = '';
-      }
-    }, 500);
-  };
+  }, [isHover]);
 
   const handleSwipeClick = (direction: number) => {
-    const newIndex = currentIndex + direction;
-
-    if (newIndex === 4) {
-      moveToNthBanner(1);
-    }
-
-    if (newIndex === 0) {
-      moveToNthBanner(3);
-    }
-
-    setCurrentIndex((prevCurrentIndex) => prevCurrentIndex + direction);
+    setCurrentIndex((prevCurrentIndex) => {
+      const newIndex = prevCurrentIndex + direction;
+      if (newIndex === 4) {
+        setTimeout(() => {
+          if (bannerRef.current !== null) {
+            bannerRef.current.style.transition = 'none';
+            bannerRef.current.style.transform = 'translateX(-100%)';
+          }
+          setCurrentIndex(1);
+        }, 500);
+        return newIndex;
+      }
+      if (newIndex === 0) {
+        setTimeout(() => {
+          if (bannerRef.current !== null) {
+            bannerRef.current.style.transition = 'none';
+            bannerRef.current.style.transform = 'translateX(-300%)';
+          }
+          setCurrentIndex(3);
+        }, 500);
+        return newIndex;
+      }
+      return newIndex;
+    });
 
     if (bannerRef.current !== null) {
       bannerRef.current.style.transition = 'all 0.5s ease-in-out';
     }
-  };
-
-  const handleMouseEnter = () => {
-    setIsHover(true);
-  };
-
-  const handleMouseLeave = () => {
-    setIsHover(false);
   };
 
   return (
